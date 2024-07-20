a2a5d5c21e834ad73d8ba6246df636465bb5a637
raju <rajupraaa1234@gmail.com>
2024-07-21 01:30:23 +0530

    minor changes

--- app/src/main/java/com/example/ltcworkspacereservationapplication/presentation/composable/HomePage/CustomGridItem.kt
+++ app/src/main/java/com/example/ltcworkspacereservationapplication/presentation/composable/HomePage/CustomGridItem.kt
@@ -1,35 +1,24 @@
 package com.example.ltcworkspacereservationapplication.presentation.composable.HomePage
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.example.ltcworkspacereservationapplication.presentation.composable.CustomButton
-import com.example.ltcworkspacereservationapplication.presentation.composable.CustomDatePickerDialog
 import com.example.ltcworkspacereservationapplication.presentation.utils.Spacing
-import com.example.ltcworkspacereservationapplication.presentation.utils.color.AppColor
 
 
 @Composable
