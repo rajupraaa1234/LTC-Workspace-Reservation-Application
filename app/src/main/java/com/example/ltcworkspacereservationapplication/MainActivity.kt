af0809d481651a0c320444bfe93260a76f2dd842
raju <rajupraaa1234@gmail.com>
2024-07-20 16:38:11 +0530

    bottom Navigation added

--- app/src/main/java/com/example/ltcworkspacereservationapplication/MainActivity.kt
+++ app/src/main/java/com/example/ltcworkspacereservationapplication/MainActivity.kt
@@ -18,11 +18,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import com.example.ltcworkspacereservationapplication.presentation.composable.CustomDropdownMenu
 import com.example.ltcworkspacereservationapplication.presentation.mvvm.AppIntent
@@ -37,23 +44,98 @@ class MainActivity : ComponentActivity() {
         val viewModel: ReservationViewModel by viewModels()
         enableEdgeToEdge()
         setContent {
-            App(viewModel)
+            BottomTabNavigation(viewModel)
         }
     }
 }
 
+
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
-private fun App(viewModel: ReservationViewModel) {
+private fun BottomTabNavigation(viewModel: ReservationViewModel) {
+    val selectedTab = remember { mutableStateOf(0) }
+
+    val TAG = "BottomTabNavigation"
+    Log.d(TAG, "BottomTabNavigation 1: ${selectedTab.value}")
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigation(
+                backgroundColor = Color.White,
+                contentColor = AppColor.primaryColor,
+            ) {
+                BottomNavigationItem(
+                    icon = { Icon(painterResource(R.drawable.home), contentDescription = null) },
+                    label = {
+                        Text(
+                            "Home",
+                            color = if (selectedTab.value == 0) AppColor.primaryColor else Color.Gray
+                        )
+                    },
+                    selected = selectedTab.value == 0,
+                    onClick = {
+                        selectedTab.value = 0
+                        Log.d(TAG, "BottomTabNavigation: ${selectedTab.value}")
+                    },
+                    selectedContentColor = AppColor.primaryColor,
+                    unselectedContentColor = Color.Gray
+                )
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            painterResource(R.drawable.qr_code_scan),
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(
+                            "Scan",
+                            color = if (selectedTab.value == 1) AppColor.primaryColor else Color.Gray
+                        )
+                    },
+                    selected = selectedTab.value == 1,
+                    onClick = {
+                        selectedTab.value = 1
+                        Log.d(TAG, "BottomTabNavigation: ${selectedTab.value}")
+                    },
+                    selectedContentColor = AppColor.primaryColor,
+                    unselectedContentColor = Color.Gray
+                )
+                BottomNavigationItem(
+                    icon = { Icon(painterResource(R.drawable.history), contentDescription = null) },
+                    label = {
+                        Text(
+                            "History",
+                            color = if (selectedTab.value == 2) AppColor.primaryColor else Color.Gray
+                        )
+                    },
+                    selected = selectedTab.value == 2,
+                    onClick = {
+                        selectedTab.value = 2
+                        Log.d(TAG, "BottomTabNavigation: ${selectedTab.value}")
+                    },
+                    selectedContentColor = AppColor.primaryColor,
+                    unselectedContentColor = Color.Gray
+                )
+            }
+        }
+    )
+    { innerPadding -> App(modifier = Modifier.padding(innerPadding), viewModel) }
+}
+
+
+@SuppressLint("StateFlowValueCalledInComposition")
+@Composable
+private fun App(modifier: Modifier, viewModel: ReservationViewModel) {
     val userName = viewModel.uiState.value.employeeName
 
     Column(
-        modifier = Modifier
+        modifier = modifier
             .padding(
-                top = Spacing.Size_50,
+                top = Spacing.Size_20,
                 start = Spacing.Size_20,
                 end = Spacing.Size_20,
-                bottom = Spacing.Size_20
+                bottom = Spacing.Size_0
             )
     ) {
         Text(
